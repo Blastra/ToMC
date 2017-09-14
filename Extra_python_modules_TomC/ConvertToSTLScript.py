ab2fd86452f6704bb2de8dfe849901d7f7537325
arad <rakholl@gmail.com>
2017-09-14 09:45:42 +0300

    Added library and list for vertices and indices for replicating the targeted form from the scene

--- Extra_python_modules_TomC/ConvertToSTLScript.py
+++ Extra_python_modules_TomC/ConvertToSTLScript.py
@@ -1,7 +1,18 @@
 import bpy
+import sys
 
-destFilePath = "/home/arad/Desktop/TestiSTLPalkki.stl"
+print("sys.argv[2:] "+str(sys.argv[0:]))
 
-bpy.ops.export_mesh.stl(filepath=destFilePath,check_existing=True,axis_forward="Y",axis_up="Z",filter_glob="*.stl",use_selection=False,global_scale=1.0,use_scene_unit=False,ascii=False,use_mesh_modifiers=True,batch_mode="OFF")
+#From the list of arguments mesh vertices passed from BGE can be parsed
+
+verts = sys.argv[5]
+
+#From the list of arguments mesh faces passed from BGE can be parsed
+
+faces = sys.argv[6]
+
+#destFilePath = "/home/arad/Desktop/TestiSTLPalkki.stl"
+
+#bpy.ops.export_mesh.stl(filepath=destFilePath,check_existing=True,axis_forward="Y",axis_up="Z",filter_glob="*.stl",use_selection=False,global_scale=1.0,use_scene_unit=False,ascii=False,use_mesh_modifiers=True,batch_mode="OFF")
 
 bpy.ops.wm.quit_blender()
